dc0ef4ac2a824132fb5433f38a84cbb7109f843e
Martins Canders <chakars13@gmail.com>
2016-03-30 16:29:43 +0100

    Pats Pirmais

---
+++ MissionControl/src/FirstView.java
@@ -0,0 +1,96 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.util.LinkedList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.event.SelectEvent;
+import gov.nasa.worldwind.event.SelectListener;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.Material;
+import gov.nasa.worldwind.render.Polyline;
+import gov.nasa.worldwind.render.SurfaceEllipse;
+
+public class FirstView {
+	public static RenderableLayer layer = null;
+	FirstView(){
+		
+		
+		System.out.println("Started");
+		//create a WorldWind main object
+				WorldWindowGLCanvas worldWindCanvas = new WorldWindowGLCanvas();
+				worldWindCanvas.setModel(new BasicModel());
+				
+				//build Java swing interface
+				JFrame frame = new JFrame("World Wind");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.add(worldWindCanvas);
+				frame.setSize(800,600);
+				frame.setVisible(true);
+				
+				// Create an empty layer, give it a name
+		        layer = new RenderableLayer();
+		        layer.setName("Interactive ellipses");
+		        
+		     // Use the built-in renderer code to draw an ellipse (near Perth, Australia).
+		        SurfaceEllipse e1 = new SurfaceEllipse(
+		                LatLon.fromDegrees(-31.0d, 115.0d), // reference position - order is LATITUDE, LONGITUDE
+		                100000.0d, // major radius
+		                50000.0d, // minor radius
+		                Angle.fromDegrees(45.0d) // rotation
+		                );
+		        
+		        e1.getAttributes().setInteriorMaterial(Material.ORANGE);
+		        layer.addRenderable(e1);
+		        
+		        worldWindCanvas.getModel().getLayers().add(layer);
+				worldWindCanvas.addSelectListener(new SelectListener(){
+					
+					// triggered when any selection event occurs
+		            public void selected(SelectEvent event) {
+	System.out.println("*** SELECT EVENT FIRED **** (" + event.getEventAction() + ")");
+					
+					
+					
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+			/*	//create some "Position" to build a polyline
+				LinkedList<Position> list = new LinkedList<Position>();
+				for(int i = 0 ; i < 90 ; i++) {
+					//in this case, points are in geographic coordinates.
+					//If you are using cartesian coordinates, you have to convert them to geographic coordinates.
+					//Maybe, there are some functions doing that in WWJ API...
+					list.add(Position.fromDegrees(i,0.0,i*20000));
+				}
+				
+				//create "Polyline" with list of "Position" and set color / thickness
+				Polyline polyline = new Polyline(list);
+				polyline.setColor(Color.RED);
+				polyline.setLineWidth(3.0);
+				
+				//create a layer and add Polyline
+				RenderableLayer layer = new RenderableLayer();
+				layer.addRenderable(polyline);
+				
+				//add layer to WorldWind
+				worldWindCanvas.getModel().getLayers().add(layer);*/
+				
+				
+	
+	
+	});
+	}
+}
---
+++ MissionControl/src/MainClass.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.util.measure.AreaMeasurer;
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
+import gov.nasa.worldwindx.examples.Balloons;
+import gov.nasa.worldwindx.examples.Cones;
+import gov.nasa.worldwindx.examples.DraggingShapes;
+import gov.nasa.worldwindx.examples.Markers;
+import gov.nasa.worldwindx.examples.analytics.AnalyticSurface;
+import gov.nasa.worldwindx.examples.analytics.AnalyticSurfaceDemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}

11d952d62e0ff57aef251a61c63daee82a9111a8
McCanders <martinscanders@192.168.1.102>
2016-03-31 18:08:20 +0100

    lalaa

--- MissionControl/src/FirstView.java
+++ MissionControl/src/FirstView.java
@@ -1,42 +1,57 @@
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Frame;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.Model;
+import gov.nasa.worldwind.WorldWind;
+import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
 import gov.nasa.worldwind.event.SelectEvent;
 import gov.nasa.worldwind.event.SelectListener;
 import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.LatLon;
 import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.MarkerLayer;
 import gov.nasa.worldwind.layers.RenderableLayer;
 import gov.nasa.worldwind.render.Material;
 import gov.nasa.worldwind.render.Polyline;
 import gov.nasa.worldwind.render.SurfaceEllipse;
+import gov.nasa.worldwind.render.markers.BasicMarker;
+import gov.nasa.worldwind.render.markers.BasicMarkerAttributes;
+import gov.nasa.worldwind.render.markers.BasicMarkerShape;
+import gov.nasa.worldwind.render.markers.Marker;
+import gov.nasa.worldwind.render.markers.MarkerAttributes;
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
 
 public class FirstView {
+	
+	protected WorldWindow worldWindCanvas;
 	public static RenderableLayer layer = null;
 	FirstView(){
 		
 		
 		System.out.println("Started");
 		//create a WorldWind main object
-				WorldWindowGLCanvas worldWindCanvas = new WorldWindowGLCanvas();
+				WorldWindow worldWindCanvas = new WorldWindowGLCanvas();
 				worldWindCanvas.setModel(new BasicModel());
 				
 				//build Java swing interface
 				JFrame frame = new JFrame("World Wind");
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(worldWindCanvas);
+				frame.add((Component) worldWindCanvas);
 				frame.setSize(800,600);
 				frame.setVisible(true);
 				
-				worldWindCanvas.getInputHandler().addMouseListener(new MouseAdapter() {
+				((WorldWindow)worldWindCanvas).getInputHandler().addMouseListener(new MouseAdapter() {
 				    @Override
 				    public void mouseClicked(MouseEvent pE) {
 
@@ -46,6 +61,7 @@ public class FirstView {
 				        if(aCurrentPosition != null) {
 
 				            System.out.println("Current Pos= " + aCurrentPosition);
+				            
 
 				        } else {
 
@@ -55,6 +71,10 @@ public class FirstView {
 				    }
 				});
 				
+				
+				
+				
+
 			/*	// Create an empty layer, give it a name
 		        layer = new RenderableLayer();
 		        layer.setName("Interactive ellipses");
@@ -216,4 +236,7 @@ public class FirstView {
     }
 });*/
 	}
+	// Create the default model as described in the current worldwind properties.
+   
+	
 }

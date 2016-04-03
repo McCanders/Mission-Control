a274dfd8642bd130368c5f8cb97da6e9ccc31937
McCanders <martinscanders@192.168.1.102>
2016-04-03 19:56:45 +0100

    Picking objects

--- MissionControl/src/FirstView.java
+++ MissionControl/src/FirstView.java
@@ -3,6 +3,7 @@ import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
@@ -12,15 +13,20 @@ import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
 import gov.nasa.worldwind.event.SelectEvent;
 import gov.nasa.worldwind.event.SelectListener;
 import gov.nasa.worldwind.geom.Position;
-import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.layers.Earth.BMNGOneImage;
+import gov.nasa.worldwind.render.AnnotationAttributes;
 import gov.nasa.worldwind.render.PointPlacemark;
 import gov.nasa.worldwind.render.Polyline;
 import gov.nasa.worldwind.util.BasicDragger;
+import gov.nasa.worldwindx.applications.sar.TrackPanel;
+import gov.nasa.worldwind.layers.*;
 
 public class FirstView {
 	
 	protected WorldWindow worldWindCanvas;
 	static RenderableLayer layer = null;
+	Position mouseCurrentPosition = null;
+	
 	
 	FirstView(){
 		
@@ -44,6 +50,9 @@ public class FirstView {
 				//List to hold positions and use them later for creating picture 
 				LinkedList<Position> mousePositionOnMap = new LinkedList<>();
 				//for now layer shared with others so screen has only one layer on top 
+				// Create one set of layers.
+	            
+	            
 				layer = new RenderableLayer();
 				
 				//adding mouse listener to window
@@ -52,25 +61,85 @@ public class FirstView {
 				    public void mouseClicked(MouseEvent pE) {
                         //getting position from current mouse click
 				    final Position mouseCurrentPosition = worldWindCanvas.getCurrentPosition();
+				    
 
 				        //Or whatever work:      
-				        if(mouseCurrentPosition != null) {
+				        if(mouseCurrentPosition != null && (mousePositionOnMap.contains(mouseCurrentPosition) != true )) {
 				        	//after null check to make sure coordinates are not null avoiding null point exceptions
-				        	mousePositionOnMap.add(mouseCurrentPosition);
-
+				        	
 				        	//constructor for creating placeMarks from mouse position
 				        	PointPlacemark pointPlacemarkOnMapList = new  PointPlacemark (mouseCurrentPosition);
+				        	mousePositionOnMap.add(mouseCurrentPosition);
+				        	
+				        	
 			    				
 			    			
 			    			layer.addRenderable(pointPlacemarkOnMapList);
+			    			 if (mousePositionOnMap.size()>1){
+		 				        	Polyline polyline = new Polyline(mousePositionOnMap);
+		 				        	
+		 				        	
+		 				        	polyline.setColor(Color.RED);
+		 				        	polyline.setLineWidth(2);
+		 				        	layer.addRenderable(polyline);}
 			    			
-			    			//creating poly line checking if more than one entry in list for line
+			    			//adding select listener to window 
+							worldWindCanvas.getInputHandler().addSelectListener(new SelectListener(){
+									   //running method if for selection 
+						    			public void selected(SelectEvent event){
+						    				//checking what is selected
+						    				if(event.getEventAction().equals(SelectEvent.LEFT_CLICK) && event.hasObjects() ){
+						    					//logic what to do after selection is finished
+                                             final   Object pickedObject = event.getTopObject();
+                                             
+                                                if(pickedObject instanceof  PointPlacemark){
+                                                	
+                                                	System.out.println("");
+                                                }
+						    					layer.dispose();
+						    				
+						    					
+						    					
+						    					
+						    					
+						    					//end of polyline creating
+						    				Position placemarkOnMap =	pointPlacemarkOnMapList.getPosition();
+						    						Position dragEndPosition = worldWindCanvas.getCurrentPosition();
+							    					if(mousePositionOnMap.contains(mouseCurrentPosition)) {
+							    						int indexToReplace = mousePositionOnMap.indexOf(mouseCurrentPosition);
+							    						
+							    						mousePositionOnMap.set(indexToReplace, dragEndPosition);
+							    						
+							    						System.out.println("Position found" + mousePositionOnMap.indexOf(mouseCurrentPosition));
+							    						
+							    					};
+						    					 
+						    					
+						    					
+						    				}//ends logic for first type (if) statement
+						    				//adding one more selection criteria
+						    				else if (event.getEventAction().equals(SelectEvent.DRAG) && event.hasObjects() && event.getTopObject() instanceof  PointPlacemark  ){
+						    					
+						    					
+						    					
+						    					
+						    					
+						    					//TODO
+						    					
+						    				}//end second if (else if) logic
+						    			
+						    			}//ends selected method
+									
+							
+									
+						    			});//ends select listeners		
+							
 	    					
 							
 				        	
 							
 							//adding to window
-							worldWindCanvas.getModel().getLayers().add(layer);
+							
 				        	
 				            System.out.println("Current Pos= " + mouseCurrentPosition);
 				            
@@ -81,7 +150,7 @@ public class FirstView {
 				        
 				        else {
 
-				            System.out.println("Current Pos is null!");
+				            System.out.println("NOTHING added this position is on list, or other reason");
 
 				        }//ends else loop it position is null 
 				       
@@ -90,39 +159,7 @@ public class FirstView {
 				    
 				});//ends mouse Listener 
 				
-				//adding select listener to window 
-				worldWindCanvas.getInputHandler().addSelectListener(new SelectListener(){
-						   //running method if for selection 
-			    			public void selected(SelectEvent event){
-			    				//checking what is selected
-			    				if(event.getEventAction().equals(SelectEvent.DRAG_END) && event.hasObjects() && event.getTopObject() instanceof Polyline){
-			    					//logic what to do after selection is finished
-			    					System.out.println("Checcking markers poly drag");
-			    					
-			    					 
-			    					
-			    					
-			    				}//ends logic for first type (if) statement
-			    				//adding one more selection criteria
-			    				else if (event.getEventAction().equals(SelectEvent.DRAG_END) && event.hasObjects() && event.getTopObject() instanceof  PointPlacemark){
-			    					System.out.println("Checcking markers  drag");
-			    					
-			    					layer.dispose();
-			    					 if (mousePositionOnMap.size()>1){
-				 				        	Polyline polyline = new Polyline(mousePositionOnMap);
-				 				        	polyline.setColor(Color.RED);
-				 				        	polyline.setLineWidth(2);
-				 				        	layer.addRenderable(polyline);}//end of polyline creating
-			    					//TODO
-			    					
-			    				}//end second if (else if) logic
-			    			
-			    			}//ends selected method
-						
-				
-						
-			    			});//ends select listeners		
-				
+				worldWindCanvas.getModel().getLayers().add(layer);
 				
 				
 				
